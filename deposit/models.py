26439922716814aa6d411ffe7458273c3d969884
john_kamau <johnkama45@gmail.com>
2017-07-06 08:54:58 +0300

    New definition of pending registrations Transaction description

--- deposit/models.py
+++ deposit/models.py
@@ -8,14 +8,15 @@ from circle.models import CircleMember
 class Deposit(models.Model):
     DEPOSIT_CHANNELS = (
         ('MPESA', 'M-pesa'),
-        ('PESALINK', 'Pesalink')
+        ('PESALINK', 'Pesalink'),
+        ('PAYPAL', 'Paypal'),
         ('CHEQUE DEPOSIT', 'Cheque deposit'),
         ('CASH DEPOSIT', 'Cash deposit'),
-        ('CARD TRANSFER', 'Card transfer'),
-        ('PAYPAL', 'Paypal'),
+        ('CARD TRANSFER', 'Card transfer')
     )
 
     DEPOSIT_TYPE = (
+        ('REGISTRATION', 'Registration fee'),
         ('LOAN_REPAYMENT', 'Loan repayment'),
         ('SHARES', 'Shares')
     )
@@ -24,9 +25,10 @@ class Deposit(models.Model):
     amount = models.DecimalField(max_digits=12, decimal_places=2)
     channel = models.CharField(max_length=50, blank=False, choices=DEPOSIT_CHANNELS)
     deposit_type = models.CharField(max_length=20, blank=False, choices=DEPOSIT_TYPE)
+    transaction_desc = models.CharField(max_length=200, blank=False)
     time_deposited = models.DateTimeField(null=False)
     other_info = models.TextField(max_length=10000, blank=True)
-    time_created = models.DateField(null=False)
+    time_created = models.DateField(auto_now=True, null=False)
 
     class Meta:
         db_table = 'Deposit'
--- member/models.py
+++ member/models.py
@@ -5,6 +5,22 @@ from django.db import models
 GENDER_CHOICES = (('M', 'Male'), ('F', 'Female'))
 
 
+class PendingRegistration(models.Model):
+    national_id = models.CharField(max_length=20, blank=False, unique=True)
+    phone_number = models.CharField(max_length=20, blank=False, unique=True)
+    email_address = models.EmailField(max_length=100, blank=False, unique=True)
+    registered_device = models.TextField(max_length=1000, blank=True)
+    time_created = models.DateTimeField(auto_now=True)
+
+    national_id_confirmed = models.BooleanField(default=False)
+    phone_number_confirmed = models.BooleanField(default=False)
+    email_address_confirmed = models.BooleanField(default=False)
+    registration_fee_paid = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = 'PendingRegistration'
+
+
 class Member(models.Model):
     """
         A class that stores the Investor
@@ -17,12 +33,14 @@ class Member(models.Model):
     other_name = models.CharField(max_length=20, blank=True)
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
     date_of_birth = models.DateField(null=True)
-    profile_picture = models.FileField(storage='PROFILE_PICTURES')
-    registered_device = models.TextField(max_length=10000, blank=True)
+    passport_image = models.FileField(storage='MEMBER_PASSPORT_IMAGE', null=True, blank=True)
+    registered_device = models.TextField(max_length=1000, blank=True)
     occupation = models.CharField(max_length=100, blank=True)
-    other_info = models.TextField(max_length=10000, blank=True)
-    is_active = models.BooleanField(default=True)
-    time_created = models.DateTimeField(auto_now=True)
+    other_info = models.TextField(max_length=1000, blank=True)
+    time_registered = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=False)
+    has_agreed_to_terms = models.BooleanField(default=False)
+    universal_transaction_pin = models.CharField(max_length=1000, blank=True)
 
     class Meta:
         db_table = 'Member'
@@ -45,6 +63,7 @@ class Beneficiary(models.Model):
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
     relationship = models.CharField(max_length=10, choices=RELATIONSHIP_CHOICES)
     date_of_birth = models.DateField(null=True)
+    passport_image = models.FileField(storage='MEMBER_PASSPORT_IMAGE', null=True, blank=True)
     other_info = models.TextField(max_length=10000, blank=True)
     time_created = models.DateTimeField(auto_now=True)
 
@@ -75,8 +94,9 @@ class MemberBankCard(models.Model):
     member = models.ForeignKey(Member, null=False, blank=False, on_delete=models.CASCADE)
     card_number = models.CharField(max_length=20, blank=False, unique=True)
     expires_on = models.CharField(max_length=5, blank=False)
-    cvv = models.CharField(max_length=10, blank=True)
+    card_verification_value = models.CharField(max_length=10, blank=True)
     other_info = models.TextField(max_length=10000, blank=True)
+    is_active = models.BooleanField(default=True)
     time_created = models.DateTimeField(auto_now=True)
 
     class Meta:
@@ -85,7 +105,7 @@ class MemberBankCard(models.Model):
 
 class MemberActiveSession(models.Model):
     member = models.ForeignKey(Member, null=False, blank=False, on_delete=models.CASCADE)
-    session_info = models.TextField(max_length=10000, blank=True)
+    session_info = models.TextField(max_length=1000, blank=True)
     session_start_time = models.DateTimeField(auto_now=True)
     session_end_time = models.DateTimeField(null=True)
 

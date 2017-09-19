53203342fbde62f76a473718f81c129480b6d76d
sammienjihia <dilaninjihia@gmail.com>
2017-09-19 13:14:33 +0300

    Nothing to comment

--- wallet/urls.py
+++ wallet/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
    url(r'^$',views.api_root,name="api_root"),
    url(r'^wallet_to_wallet/$',views.WallettoWalletTranfer.as_view(),name="wallet-tranfer"),
    url(r'^wallet_transactions/$',views.TransactionsDetails.as_view(),name="wallet-transactions"),
-   url(r'^get_wallet_transaction/$',views.WalletTransactionDetails.as_view(),name="wallet-transaction")
-   url(r'^wallet_transactions/$',views.TransactionDetails.as_view(),name="wallet-transactions"),
+   url(r'^get_wallet_transaction/$',views.WalletTransactionDetails.as_view(),name="wallet-transaction"),
+   url(r'^wallet_transactions/$',views.TransactionsDetails.as_view(),name="wallet-transactions"),
    url(r'^mpesa_lipa_online_initiate/$', views.MpesaToWallet.as_view(), name="mpesa_lipa_online_initiate"),
    url(r'^mpesaCallbackURL/$', views.MpesaCallbackURL.as_view(), name="mpesaCallbackURL")
 ]

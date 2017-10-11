4763989dec75df746bb991653c2d0ea2f59fd45a
sammienjihia <dilaninjihia@gmail.com>
2017-10-11 17:15:28 +0300

    No comment

--- accounts/tasks.py
+++ accounts/tasks.py
@@ -1,12 +1,14 @@
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
 from app_utility import accounts_utils
+from member.models import Member
 
 
 
 @shared_task
 def save_member_contacts(user,contacts):
     instance = accounts_utils.Account()
+    user = Member.objects.get(id=user)
     instance.save_contacts(user, contacts)
     message = "Saved contacts successfully"
 

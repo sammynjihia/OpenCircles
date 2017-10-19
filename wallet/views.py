cab86a0ad076a3486da7d0182a5ed901c0072ea6
sammienjihia <dilaninjihia@gmail.com>
2017-10-19 19:23:14 +0300

    debug log files

--- wallet/views.py
+++ wallet/views.py
@@ -387,8 +387,15 @@ class MpesaC2BConfirmationURL(APIView):
             post_file.write(data)
             post_file.write("\n")
 
-        #result = json.loads(data)
+        result1 = json.loads(data)
+        with open('c2b_result1_post_file.txt', 'a') as post_file:
+            post_file.write(result1)
+            post_file.write("\n")
         result = data.json()
+        with open('c2b_result_post_file.txt', 'a') as post_file:
+            post_file.write(result)
+            post_file.write("\n")
+            
         transaction_id = result["TransID"]
         transaction_time = result["TransTime"]
         transaction_amount = result["TransAmount"]

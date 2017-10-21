9d0ecd51ded5cd36f8d5b4d45a9de8472b04d65c
sammienjihia <dilaninjihia@gmail.com>
2017-10-21 17:44:04 +0300

    Added the the b2b api request function

--- app_utility/mpesa_api_utils.py
+++ app_utility/mpesa_api_utils.py
@@ -3,8 +3,8 @@ import json
 from requests.auth import HTTPBasicAuth
 import datetime
 import base64
-#from M2Crypto import RSA, X509
-#from base64 import b64encode
+from M2Crypto import RSA, X509
+from base64 import b64encode
 
 consumer_key = "iT4xDCEGuaAbQkcOVTvjm8vGl2K3jqcz"
 consumer_secret = "Wwyu7nOkNGGDom6V"
@@ -18,12 +18,15 @@ passkey = "068807038af3ff41280527fc8f6f034a17d334b997403e46798d0fb8a171a99e"
 password_raw = bytes(shortcode + passkey + timestamp)
 password = base64.b64encode(password_raw )
 
+B2BResultURL = main_url + "/wallet/mpesaB2BResultURL/"
+B2BQueueTimeOutURL = main_url + "/wallet/mpesaB2BQueueTimeOutURL/"
 B2CResultURL = main_url + "wallet/mpesaB2CResultURL/"
 B2CQueueTimeOutURL = main_url + "wallet/mpesaB2CQueueTimeoutURL/"
 B2CPartyB = "254708374149"
 B2CPartyA = "600232"
 #B2CPartyA = "564433"
-INITIATOR_PASS  = "Safaricom232!"
+INITIATOR = "FLEMISHINFORMATICS"
+INITIATOR_PASS  = "Opencircles2017!"
 CERTIFICATE_FILE = "cert.cer"
 security_credential2 = "ju95cBXnjD1mkvo2auzEdvRTD0k3CpfvZes9GXzkcyEXRWzpKz6e3EutzXuM/odNhmttkRv8OxmkUDNwYYKzPyQ8Irrrzej0WU9x2t9Au0seJYmzMjpa/btlqGhmpon1xQ0TzWdBQdQWB3qMHOAixZIYUGqY26YLH8gAiCAOTvAVTLE5x7zhUZuvRbWr2OZ26rpYSreIchfIUEpugmm+cazx9PKOs8dFQHS1LQWOPfyCO1Cz0rL3t/7f1AwoOOWZRNcZAgysd76HRQqrBVyrs0vqfXb6i8MHu6WQ0yWa9yzGMhyxtARtF7VJAPfQSUSusc8E1ADUvdABymviwehV7w=="
 
@@ -39,16 +42,16 @@ class MpesaUtils():
         return access_token
 
     # generating security credentials
-    # def encryptInitiatorPassword(self):
-    #     cert_file = open(CERTIFICATE_FILE, 'r')
-    #     cert_data = cert_file.read()  # read certificate file
-    #     cert_file.close()
-    #     cert = X509.load_cert_string(cert_data)
-    #     # pub_key = X509.load_cert_string(cert_data)
-    #     pub_key = cert.get_pubkey()
-    #     rsa_key = pub_key.get_rsa()
-    #     cipher = rsa_key.public_encrypt(INITIATOR_PASS, RSA.pkcs1_padding)
-    #     return b64encode(cipher)
+    def encryptInitiatorPassword(self):
+        cert_file = open(CERTIFICATE_FILE, 'r')
+        cert_data = cert_file.read()  # read certificate file
+        cert_file.close()
+        cert = X509.load_cert_string(cert_data)
+        # pub_key = X509.load_cert_string(cert_data)
+        pub_key = cert.get_pubkey()
+        rsa_key = pub_key.get_rsa()
+        cipher = rsa_key.public_encrypt(INITIATOR_PASS, RSA.pkcs1_padding)
+        return b64encode(cipher)
 
 
     def mpesa_online_checkout(self, amount, phone_number):
@@ -94,3 +97,26 @@ class MpesaUtils():
         response = requests.post(api_url, json = request, headers=headers)
         result = response.json()
         return  result
+
+    def mpesa_b2b_checkout(self, amount, account_number, paybill_number):
+        access_token = self.get_access_token()
+        api_url = "https://api.safaricom.co.ke/mpesa/b2b/v1/paymentrequest"
+        headers = {"Authorization": "Bearer %s" % access_token}
+        request = {
+            "Initiator": INITIATOR,
+            "SecurityCredential": self.encryptInitiatorPassword(),
+            "CommandID": "BusinessPayBill",
+            "SenderIdentifierType": "4",
+            "RecieverIdentifierType": "4",
+            "Amount": amount,
+            "PartyA": shortcode,
+            "PartyB": paybill_number,
+            "AccountReference": account_number,
+            "Remarks": "Business to business transaction ",
+            "QueueTimeOutURL": B2BQueueTimeOutURL,
+            "ResultURL": B2BResultURL
+        }
+
+        response = requests.post(api_url, json=request, headers=headers)
+
+        print(response.text)

cf315e20215ce895f2442805f2b139b728cd0c94
sammienjihia <dilaninjihia@gmail.com>
2017-10-30 17:21:40 +0300

    Added new function

--- loan/tasks.py
+++ loan/tasks.py
@@ -2,6 +2,8 @@ from __future__ import absolute_import, unicode_literals
 from celery import shared_task
 from app_utility import loan_utils
 from loan.models import GuarantorRequest
+from circle.models import Circle
+from member.models import Member
 
 
 
@@ -17,3 +19,21 @@ def unlocking_guarantors_shares(guarantors_id, shares_desc):
         post_file.write("\n")
 
     return message
+
+@shared_task
+def updating_loan_limit(circle_id, member_id):
+    instance = loan_utils.Loan()
+    circle = Circle.objects.get(id=circle_id)
+    member = Member.objects.get(id=member_id)
+    instance.update_loan_limit(circle, member)
+    log_message = "Updated loan limit successfully"
+
+    with open('celery_updating_loan_limit_worker_file.txt', 'a') as post_file:
+        post_file.write(str(circle_id))
+        post_file.write("\n")
+        post_file.write(str(member_id))
+        post_file.write("\n")
+
+    return log_message
+
+

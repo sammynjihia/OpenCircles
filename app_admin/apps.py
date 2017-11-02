895f9ee24b0c0df3425bdb3f1f72ae13cdd57d46
john_kamau <johnkama45@gmail.com>
2017-11-02 18:52:20 +0300

    admin dashboard

---
+++ app_admin/apps.py
@@ -0,0 +1,7 @@
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class AppAdminConfig(AppConfig):
+    name = 'app_admin'
---
+++ app_admin/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def create_admin(request):
+    pass
+
+
+def lock_admin_acc(request):
+    pass
+
+
+def login_admin(request):
+    pass
+
+

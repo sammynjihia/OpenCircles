037ab53788e08f0a5c89c7d59227aa2e5823f4bb
sammienjihia <dilaninjihia@gmail.com>
2017-11-03 14:19:30 +0300

    Added the Adminmpesa transaction log model to log all the mpesa transactions happening

--- wallet/models.py
+++ wallet/models.py
@@ -61,3 +61,9 @@ class MpesaTransaction_logs(models.Model):
     OriginatorConversationID = models.CharField(max_length=100, unique=True)
     ResultCode = models.CharField(max_length=5)
     ResultDesc = models.CharField(max_length=250)
+
+
+class AdminMpesaTransaction_logs(models.Model):
+    TransactioID = models.CharField(max_length=100, unique=True)
+    TransactionType = models.CharField(max_length=15)
+    Response = models.TextField(max_length=1000)

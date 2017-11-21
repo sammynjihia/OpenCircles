4ca6d755369b1f27caf790d85459cc1cad4b15fb
jeytembu <jeytembu@gmail.com>
2017-11-21 17:33:01 +0300

    fixes

--- OpenCircles/settings.py
+++ OpenCircles/settings.py
@@ -185,7 +185,7 @@ CELERY_TIMEZONE = 'Africa/Nairobi'
 CELERY_BEAT_SCHEDULE = {
     'task-number-one': {
             'task': 'member.tasks.send_frequent_invitations',
-            'schedule': crontab(hour=12, minute=45)
+            'schedule': crontab(hour=14, minute=50)
         }
 }
 

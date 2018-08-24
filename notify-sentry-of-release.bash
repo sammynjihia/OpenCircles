67d9084988fc01fe838d937ef843ee4748446628
sammienjihia <dilaninjihia@gmail.com>
2018-08-24 21:13:04 +0300

    added sentry api key to the bash script

--- notify-sentry-of-release.bash
+++ notify-sentry-of-release.bash
@@ -3,7 +3,7 @@
 # Create a Sentry API key with project:write permissions from under your Sentry organization's settings.
 # Set the following environment variables.
 
-# SENTRY_TOKEN         (Required) Sentry Token
+# SENTRY_API_KEY        (Required) Sentry Token
 # SENTRY_ORGANIZATION    (Optional) Slug (aka name) for the Sentry organization.
 #                           If not provided, this script assumes Sentry matches Bitbucket organization.
 #                           Example: "demo"
@@ -20,7 +20,7 @@ if [ -z "${SENTRY_PROJECT}" ]; then
 fi
 
 curl https://sentry.io/api/0/organizations/${SENTRY_ORGANIZATION}/releases/ \
-    -H "Authorization: Bearer $SENTRY_TOKEN" \
+    -H "Authorization: Bearer ${SENTRY_API_KEY}" \
     -X POST \
     -H "Content-Type:application/json" \
     -d "{\"version\":\"${BITBUCKET_COMMIT}\",\"ref\":\"${BITBUCKET_BRANCH}\",\"projects\": [\"${SENTRY_PROJECT}\"]}"
